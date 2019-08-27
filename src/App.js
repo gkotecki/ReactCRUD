6f29adf2bdd91c284c854bfb4e75bcee64fa8427
gkotecki <gabrielkotecki@gmail.com>
2019-08-27 11:52:56 -0300

    up to final part

--- src/App.js
+++ src/App.js
@@ -1,58 +1,70 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import ProductItem from './ProductItem';
-import AddProduct from './AddProduct';
+import ProductItem from "./ProductItem";
+import AddProduct from "./AddProduct";
 
 const products = [
-	{
-		name: 'iPad',
-		price: 200
-	},
-	{
-		name: 'iPhone',
-		price: 650
-	}
+  {
+    name: "iPad",
+    price: 200
+  },
+  {
+    name: "iPhone",
+    price: 650
+  }
 ];
 
-localStorage.setItem('products', JSON.stringify(products));
+localStorage.setItem("products", JSON.stringify(products));
 
 class App extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      products:JSON.parse(localStorage.getItem('products'))
+      products: JSON.parse(localStorage.getItem("products"))
     };
 
     this.onAdd = this.onAdd.bind(this);
     this.onDelete = this.onDelete.bind(this);
+    this.onEditSubmit = this.onEditSubmit.bind(this);
   }
 
-  componentWillMount(){
+  componentWillMount() {
     const products = this.getProducts();
-    this.setState({products});
+    this.setState({ products });
   }
 
-  getProducts(){
+  getProducts() {
     return this.state.products;
   }
 
-  onAdd(name, price){
+  onAdd(name, price) {
     const products = this.getProducts();
     products.push({
       name,
       price
     });
-    this.setState({products});
+    this.setState({ products });
   }
 
-  onDelete(name){
+  onDelete(name) {
     const products = this.getProducts();
-    const filteredProducts = products.filter(product=>{
+    const filteredProducts = products.filter(product => {
       return product.name !== name;
     });
-    this.setState({products:filteredProducts});
+    this.setState({ products: filteredProducts });
+  }
+
+  onEditSubmit(name, price, originalName) {
+    let products = this.getProducts();
+    products = products.map(product => {
+      if (product.name === originalName) {
+        product.name = name;
+        product.price = price;
+      }
+      return product;
+    });
+    this.setState({ products });
   }
 
   render() {
@@ -60,22 +72,18 @@ class App extends React.Component {
       <div className="App">
         <h1>Products Manager</h1>
 
-        <AddProduct
-            onAdd={this.onAdd}
-        />
+        <AddProduct onAdd={this.onAdd} />
 
-        {
-          this.state.products.map(product=>{
-            return (
-              <ProductItem
-                key={product.name}
-                {...product}
-                onDelete={this.onDelete}
-              />
-            );
-          })
-        }
-
+        {this.state.products.map(product => {
+          return (
+            <ProductItem
+              key={product.name}
+              {...product}
+              onDelete={this.onDelete}
+              onEditSubmit={this.onEditSubmit}
+            />
+          );
+        })}
       </div>
     );
   }
--- src/ProductItem.js
+++ src/ProductItem.js
@@ -1,25 +1,62 @@
-import React from 'react';
+import React from "react";
 
 class ProductItem extends React.Component {
-	constructor(props){
-		super(props);
-		this.onDelete  = this.onDelete.bind(this);
-	}
+  constructor(props) {
+    super(props);
 
-	onDelete(){
-		this.props.onDelete(this.props.name);
-	}
+    this.state = {
+      isEdit: false
+    };
+
+    this.onDelete = this.onDelete.bind(this);
+    this.onEdit = this.onEdit.bind(this);
+    this.onEditSubmit = this.onEditSubmit.bind(this);
+  }
+
+  onDelete() {
+    this.props.onDelete(this.props.name);
+  }
+
+  onEdit() {
+    this.setState({ isEdit: true });
+  }
+
+  onEditSubmit(event) {
+    event.preventDefault();
+    this.props.onEditSubmit(this.nameInput.value, this.priceInput.value, this.props.name);
+    this.setState({ isEdit: false });
+  }
 
   render() {
-	const {name, price} = this.props;
+    const { name, price } = this.props;
 
     return (
-      <div >
-        <span>{name}</span>
-        {` | `}
-        <span>{price}</span>
-        {` | `}
-        <button onClick={this.onDelete}>Delete</button>
+      <div>
+        {this.state.isEdit ? (
+          <form onSubmit={this.onEditSubmit}>
+            <input
+              placeholder="Name"
+              ref={nameInput => (this.nameInput = nameInput)}
+              defaultValue={name}
+            />
+            <input
+              placeholder="Price"
+              ref={priceInput => (this.priceInput = priceInput)}
+              defaultValue={price}
+            />
+            <button>Save</button>
+          </form>
+        ) : (
+          <div>
+            <span>{name}</span>
+            {` | `}
+            <span>{price}</span>
+            {` | `}
+            <button onClick={this.onEdit}>Edit</button>
+            {` | `}
+            <button onClick={this.onDelete}>Delete</button>
+          </div>
+        )}
       </div>
     );
   }
