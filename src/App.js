208b573f67005a5288527e8b88a3668c05108cd1
gkotecki <gabrielkotecki@gmail.com>
2019-08-27 01:25:05 -0300

    done till 4934

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import './App.css';
+
+import ProductItem from './ProductItem';
+import AddProduct from './AddProduct';
+
+const products = [
+	{
+		name: 'iPad',
+		price: 200
+	},
+	{
+		name: 'iPhone',
+		price: 650
+	}
+];
+
+localStorage.setItem('products', JSON.stringify(products));
+
+class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            products:JSON.parse(localStorage.getItem('products'))
+        };
+
+        this.onAdd = this.onAdd.bind(this);
+        this.onDelete = this.onDelete.bind(this);
+    }
+
+    componentWillMount(){
+        const products = this.getProducts();
+        this.setState({products});
+    }
+
+    getProducts(){
+        return this.state.products;
+    }
+
+    onAdd(name, price){
+        const products = this.getProducts();
+        products.push({
+            name,
+            price
+        });
+        this.setState({products});
+    }
+
+    onDelete(name){
+        const products = this.getProducts();
+        const filteredProducts = products.filter(product=>{
+            return product.name !== name;
+        });
+        this.setState({products:filteredProducts});
+    }
+
+  render() {
+    return (
+    <div className="App">
+      <h1>Products Manager</h1>
+
+        <AddProduct
+            onAdd={this.onAdd}
+        />
+
+      {
+          this.state.products.map(product=>{
+              return (
+                <ProductItem
+                    key={product.name}
+                    {...product}
+                    onDelete={this.onDelete}
+                />
+              );
+          })
+        }
+    </div>
+    );
+  }
+}
+
+export default App;
